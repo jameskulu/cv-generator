e60ad89b04f46c2e4bdde3008538777993f63ac7
James Kulu <jameskulu55@gmail.com>
2022-01-02 22:51:40 +0545

    Changes

--- cv/views.py
+++ cv/views.py
@@ -25,22 +25,34 @@ def list_of_cv(request):
     return render(request, "cv/list.html", {"cvs": cvs})
 
 
-if platform.system() == "Windows":
-    pdfkit_config = pdfkit.configuration(
-        wkhtmltopdf=os.environ.get("WKHTMLTOPDF_BINARY", "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
-    )
-else:
-    os.environ["PATH"] += os.pathsep + os.path.dirname(sys.executable)
-    WKHTMLTOPDF_CMD = (
-        subprocess.Popen(["which", os.environ.get("WKHTMLTOPDF_BINARY", "wkhtmltopdf")], stdout=subprocess.PIPE)
-        .communicate()[0]
-        .strip()
-    )
-    pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+# if platform.system() == "Windows":
+#     pdfkit_config = pdfkit.configuration(
+#         wkhtmltopdf=os.environ.get("WKHTMLTOPDF_BINARY", "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+#     )
+# else:
+#     os.environ["PATH"] += os.pathsep + os.path.dirname(sys.executable)
+#     WKHTMLTOPDF_CMD = (
+#         subprocess.Popen(["which", os.environ.get("WKHTMLTOPDF_BINARY", "wkhtmltopdf")], stdout=subprocess.PIPE)
+#         .communicate()[0]
+#         .strip()
+#     )
+#     pdfkit_config = pdfkit.configuration(c=WKHTMLTOPDF_CMD)
 
 
 @login_required
 def generate_cv(request, id):
+
+    if platform.system() == "Windows":
+        pdfkit_config = pdfkit.configuration(
+            wkhtmltopdf=os.environ.get("WKHTMLTOPDF_BINARY", "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+        )
+    else:
+        WKHTMLTOPDF_CMD = (
+            subprocess.Popen(["which", os.environ.get("WKHTMLTOPDF_BINARY", "wkhtmltopdf")], stdout=subprocess.PIPE)
+            .communicate()[0]
+            .strip()
+        )
+        pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
     cv = CV.objects.get(id=id)
     template = loader.get_template("cv/cv.html")
