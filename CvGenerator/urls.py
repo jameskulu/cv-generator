fd4b282728ff3deb3c9da9d0f7d042e5ba88edec
James Kulu <jameskulu55@gmail.com>
2022-01-02 16:15:38 +0545

    make it better

--- CvGenerator/urls.py
+++ CvGenerator/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("Account.urls")),
-    path("/", include("cv.urls")),
+    path("", include("cv.urls")),
     path("api/cv/", include("cv.api.urls")),
 ]
