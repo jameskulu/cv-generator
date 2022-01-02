be635d4b9c4768b96f1e0a18ea1c68236102eb06
James Kulu <jameskulu55@gmail.com>
2022-01-02 16:44:48 +0545

    make it better

--- cv/views.py
+++ cv/views.py
@@ -27,24 +27,24 @@ def list_of_cv(request):
 
 @login_required
 def generate_cv(request, id):
-    if platform.system() == "Windows":
-        pdfkit_config = pdfkit.configuration(
-            wkhtmltopdf=os.environ.get("WKHTMLTOPDF_BINARY", "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
-        )
-    elif platform.system() == 'Darwin':
-        pdfkit_config = pdfkit.configuration()
-    else:
-        os.environ["PATH"] += os.pathsep + os.path.dirname(sys.executable)
-        WKHTMLTOPDF_CMD = (
-            subprocess.Popen(["which", os.environ.get("WKHTMLTOPDF_BINARY", "wkhtmltopdf")], stdout=subprocess.PIPE)
-            .communicate()[0]
-            .strip()
-        )
-        pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
-    # if platform.system() == 'Darwin':
-    #     config = pdfkit.configuration()
+    # if platform.system() == "Windows":
+    #     pdfkit_config = pdfkit.configuration(
+    #         wkhtmltopdf=os.environ.get("WKHTMLTOPDF_BINARY", "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+    #     )
+    # elif platform.system() == 'Darwin':
+    #     pdfkit_config = pdfkit.configuration()
     # else:
-    #     config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+    #     os.environ["PATH"] += os.pathsep + os.path.dirname(sys.executable)
+    #     WKHTMLTOPDF_CMD = (
+    #         subprocess.Popen(["which", os.environ.get("WKHTMLTOPDF_BINARY", "wkhtmltopdf")], stdout=subprocess.PIPE)
+    #         .communicate()[0]
+    #         .strip()
+    #     )
+    #     pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+    if platform.system() == "Darwin":
+        config = pdfkit.configuration()
+    else:
+        config = pdfkit.configuration(wkhtmltopdf="./bin/wkhtmltopdf")
     cv = CV.objects.get(id=id)
     template = loader.get_template("cv/cv.html")
     html = template.render({"cv": cv})
