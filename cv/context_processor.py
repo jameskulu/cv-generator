5fe81a8790c46c841037f57d807ca0b4b62a789c
James Kulu <jameskulu55@gmail.com>
2022-01-11 12:40:07 +0545

    Added skill chart

--- cv/context_processor.py
+++ cv/context_processor.py
@@ -6,7 +6,17 @@ from cv.models import CV
 def number_of_users_cvs(request):
     users = User.objects.all()
     cvs = CV.objects.all()
+
+    skills = []
+    for i in cvs:
+        for j in i.skills:
+            skills.append(j.lower())
+
+    my_dict = {i: skills.count(i) for i in skills}
+
     return {
-        'no_of_users': len(users),
-        'no_of_cvs': len(cvs)
+        "no_of_users": len(users),
+        "no_of_cvs": len(cvs),
+        "chart_keys": list(my_dict.keys()),
+        "chart_values": list(my_dict.values()),
     }
